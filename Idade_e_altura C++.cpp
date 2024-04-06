d21b89e27d50e4ee76990327ec8c7c6896f99706
jim-morrison-saraiva <jim13morrison13@gmail.com>
2024-04-05 21:22:57 -0300

    Idade e altura C++

---
+++ Idade_e_altura C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int idade;
+   double altura;
+   cout << "digite sua idade \n";
+   cin >> idade;
+   cout << "digite sua altura \n";
+   cin >> altura;
+   cout << "Sua idade e: " << idade << endl;
+   cout << "Sua altura e: " << altura << endl;
+
+    return 0;
+}
