a4972116f8939992380b05a5650c2a91d9b90395
jim morrison <jim13morrison13@gmail.com>
2024-04-10 18:11:32 -0400

    create

---
+++ aula10.py
@@ -0,0 +1,11 @@
+nome = input("Digite seu nome: ")
+posicao = int(input("Digite a posição que deseja adicionar a letra: "))
+print(nome[i])
+
+if nome[posicao] == "s" or "S":
+    print(nome[:posicao]+"r"+nome[posicao+1:])
+elif nome[posicao] == "m" or "M": 
+    print(nome[:posicao]+"n"+nome[posicao+1:])
+else:
+    print(nome[:posicao]+nome[posicao+1:])
+#jim
