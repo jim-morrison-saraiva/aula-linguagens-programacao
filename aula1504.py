d717181a9479f0689f039248ac2c008783bc9096
jim morrison <jim13morrison13@gmail.com>
2024-04-15 22:33:36 -0300

    vai

---
+++ aula1504.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+busca = input('qual nome deseja pesquisar? ')
+busca_nome, busca_sobrenome = busca.split(' ')
+busca_nome = busca_nome.lower()
+busca_sobrenome = busca_sobrenome.lower()
+
+with open ("alunos.csv", "r") as arquivo:
+    next (arquivo) 
+    lista_linhas = arquivo.readlines()
+
+    for linha in lista_linhas:
+        nome_chamada, data_nascimento, matricula, = linha.strip('\n').split(',')
+        nome, sobrenome, numero = nome_chamada.split(' ')
+        if (nome.lower() == busca_nome and sobrenome.lower() == busca_sobrenome):
+            data_nascimento = datetime. strptime(data_nascimento, '%d/%m/%y')
+            hoje = datetime.now()
+            idade = hoje.year - data_nascimento.year
+            print ('nome>', nome_chamada, 'idade', idade)
